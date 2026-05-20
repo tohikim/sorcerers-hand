04b94488cc719b1871189964235164edfd84d86f
Tohi Kim <tohikimbusiness@gmail.com>
2026-05-20 21:59:13 +0400

    feat(app): add multiple decks

--- src/utils/get-deck.ts
+++ src/utils/get-deck.ts
@@ -1,11 +1,12 @@
 import { figures, suits } from "../constants/cards";
+import { NUMBER_OF_DECKS } from "../constants/variables";
 
 export const getDeck = () => {
   const deck = [];
 
-  for (let i = 0; i < suits.length; i++) {
+  for (let i = 0; i < suits.length * NUMBER_OF_DECKS; i++) {
     for (let j = 0; j < figures.length; j++) {
-      deck.push(figures[j] + suits[i]);
+      deck.push(figures[j] + suits[i % suits.length]);
     }
   }
   return deck;
